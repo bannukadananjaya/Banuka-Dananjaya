aeab123b1a603a564de91447ffdc94c1e5c95bbd
Subasinghe S.A.B.D <banukasubasinghe13@gmail.com>
2023-12-03 16:22:28 +0530

    add commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -34,7 +34,10 @@ const Sidebar = () => {
               <BsPerson size={20}/>
               <span className='pl-4'>Resume</span>
               </a>
-            
+              <a onClick={handleNav} href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 duration-200'>
+              <AiOutlineMail size={20}/>
+              <span className='pl-4'>Contact</span>
+              </a>
             </div>
           )
           :(
